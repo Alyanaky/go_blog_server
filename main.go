d532c6b03c35541e8d000e2c2901e77284b357c1
Alyanaky <007izosimov@gmail.com>
2025-05-05 19:53:23 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
+package main
+
+import (
+    "html/template"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "path/filepath"
+    "strings"
+)
+
+type Post struct {
+    Title   string
+    Content string
+}
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
 
