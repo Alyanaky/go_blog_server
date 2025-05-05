027c6b5faabeb4320132bef4ab96d64193becb3b
Alyanaky <007izosimov@gmail.com>
2025-05-05 20:59:14 +0500

    Update main.go

--- main.go
+++ main.go
@@ -16,3 +16,53 @@ type Post struct {
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
+func main() {
+    http.HandleFunc("/", indexHandler)
+
+    http.HandleFunc("/post/", postHandler)
+
+    log.Println("Starting server on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+    files, err := filepath.Glob("posts/*.md")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    var posts []Post
+    for _, file := range files {
+        content, err := ioutil.ReadFile(file)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        lines := strings.SplitN(string(content), "\n", 2)
+        posts = append(posts, Post{Title: lines[0], Content: lines[1]})
+    }
+
+    templates.ExecuteTemplate(w, "index.html", posts)
+}
+
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+
+    postName := strings.TrimPrefix(r.URL.Path, "/post/") + ".md"
+    content, err := ioutil.ReadFile("posts/" + postName)
+    if err != nil {
+        http.NotFound(w, r)
+        return
+    }
+
+    lines := strings.SplitN(string(content), "\n", 2)
+    post := Post{Title: lines[0], Content: lines[1]}
+
+    templates.ExecuteTemplate(w, "post.html", post)
+}
